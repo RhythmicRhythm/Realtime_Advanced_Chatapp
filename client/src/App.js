c17ce23885953548d3b592a3f9f7200c9d9f681b
rhythmicrhythm <Okubadejorh@gmail.com>
2023-01-23 18:36:28 +0100

    Login Route UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 
  function App() {
@@ -8,7 +8,7 @@ import Register from "./pages/Register";
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />} />
-          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/login" element={<Login />} />
           {/* <Route path="/setAvatar" element={<SetAvatar />} />
           <Route path="/" element={<Chat />} /> */}
         </Routes>
