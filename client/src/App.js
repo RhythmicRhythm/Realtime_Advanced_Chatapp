2c2e8d9bacfd76870c68f3272188ea63c0153a09
rhythmicrhythm <Okubadejorh@gmail.com>
2023-01-27 23:25:06 +0100

    Set Avatar Route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
+import SetAvatar from "./components/SetAvatar";
 
  function App() {
     return (
@@ -11,7 +12,7 @@ import Chat from "./pages/Chat";
         <Route path="/" element={<Chat />} /> 
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-           {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
+           <Route path="/setAvatar" element={<SetAvatar />} />
         </Routes>
       </BrowserRouter>
     );
